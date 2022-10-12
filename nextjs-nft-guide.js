da4930caf3fd61ce908f71499ab24a4518609dc0
mmuaaz <mastermindmuaaz@gmail.com>
2022-10-12 23:28:57 +0500

    Initial commit from Create Next App

---
+++ nextjs-nft-guide.js
@@ -0,0 +1,3 @@
+/================================Writing Code for Moralis NextJS code for Front-end/;
+// RUN COMM: yarn create next-app .
+// the above commands takes some time to gather all the packages
